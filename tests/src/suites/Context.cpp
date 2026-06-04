d50e0af406d86d9c861c8bd92b20fa13c8c418ad
Adrien GIVRY <contact@adrien-givry.com>
2026-06-04 19:19:59 -0400

    Added some simple unit tests

---
+++ tests/src/suites/Context.cpp
@@ -0,0 +1,36 @@
+/**
+* @project: baregl
+* @author: Adrien Givry
+* @licence: MIT
+*/
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <common/BareGL.h>
+
+using namespace baregl;
+
+TEST_CASE( "Context created", "[context]" ) {
+	tests::baregl::RunInContext([](GLFWwindow* p_window) {
+		Context context;
+	});
+}
+
+TEST_CASE( "Context is alone", "[context]" ) {
+	tests::baregl::RunInContext([](GLFWwindow* p_window) {
+		Context context;
+		REQUIRE_THROWS( std::make_unique<baregl::Context>() );
+	});
+}
+
+TEST_CASE( "Context can be recreated", "[context]" ) {
+	tests::baregl::RunInContext([](GLFWwindow* p_window) {
+		{
+			Context context;
+		}
+		{
+			baregl::Context context;
+		}
+	});
+}
+
